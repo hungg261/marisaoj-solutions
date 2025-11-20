4fe5f240d5248d6d7f9cfc657e7a00409cc7e3b7
hungg261 <phanthanhhung.26012010@gmail.com>
2025-11-20 21:07:12 +0700

    new

---
+++ marisaoj-solutions/17.cpp
@@ -0,0 +1,27 @@
+/******************************************************************************
+Link: https://marisaoj.com/problem/17
+Code: 17
+Time (YYYY-MM-DD-hh.mm.ss): 2025-11-20-21.04.54
+*******************************************************************************/
+#include<bits/stdc++.h>
+using namespace std;
+
+signed main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    //freopen("17.INP","r",stdin);
+    //freopen("17.OUT","w",stdout);
+    long long n;
+    cin >> n;
+
+    long long sum = 0;
+    for(int i = 1; 1LL * i * i <= n; ++i){
+        if(n % i == 0){
+            sum += i;
+            if(i != n / i) sum += n / i;
+        }
+    }
+
+    cout << sum << '\n';
+
+    return 0;
+}
